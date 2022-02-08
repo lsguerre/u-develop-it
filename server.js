fe6b6de55c6441601666581e36dfa8652d528bed
Opal <guerre5lily@gmail.com>
2022-02-07 22:59:30 -0800

    add crud routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2')
 
 const express = require('express');
-
+const inputCheck = require('./utils/inputCheck')
 const PORT = process.env.PORT || 3001;
 const app = express()
 
@@ -21,37 +21,83 @@ const db = mysql.createConnection(
     console.log('connected to the election database!')
 )
 
-db.query(`SELECT * FROM candidates WHERE id = 15`, (err,row) => {
-    if (err) {
-        console.log(err)
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Candidate not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+
+  // Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
     }
-    console.log(row)
-})
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+const params = [body.first_name, body.last_name, body.industry_connected];
 
-//delete a candidtate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err)
-//     }
-//     console.log(result)
-// })
-
-//create candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//             VALUES (?, ?, ?, ?)`
-// const params = [1, 'Ronald', 'Firbank', 1]
-
-// db.query(sql, params, (err, result) => {
-//     if(err) {
-//         console.log(err)
-//     }
-//     console.log(result)
-// })
+db.query(sql, params, (err, result) => {
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    message: 'success',
+    data: body
+  });
+});
+  });
 
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows)
-// })
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
 
 //catchall
 app.use((req, res) => {
